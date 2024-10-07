a1cd5ab2164a6707d0ef22006ccf15b0e02cc14b
JJayexd <kontakt.emilbj@gmail.com>
2024-10-07 10:43:12 +0200

    cancer

--- assets/js/bus.js
+++ assets/js/bus.js
@@ -1,25 +1,60 @@
+// function getRejseplan() {
+//     fetch('https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=851973402&rttime&format=json&useBus=1')
+//         .then((response) => {
+//             if (!response.ok) {
+//                 throw new Error(`${response.status}`)
+//             }
+
+//             console.log(response)
+//             return response.json
+//         })
+
+//         .then((data) => {
+//             if (data && isArray(data)) {
+//                 console.log(data)
+//             }
+//             console.log(data)
+//         })
+
+//         .catch((error) => {
+//             console.log(error)
+//         })
+// }
+
 function getRejseplan() {
     fetch('https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=851973402&rttime&format=json&useBus=1')
         .then((response) => {
             if (!response.ok) {
-                throw new Error(`${response.status}`)
+                throw new Error(`${response.status}`);
             }
 
             console.log(response)
-            return response.json
+            return response.json();
         })
-
         .then((data) => {
-            if (data && isArray(data)) {
-                console.log(data)
-            }
-            console.log(data)
-        })
+            const container = document.getElementById('bus');
+            container.innerHTML = '';
+    
+            if (data && Array.isArray(data)) {
+                data.forEach(item => {
 
-        .catch((error) => {
-            console.log(error)
+                    const departureInfo = document.createElement('div');
+                    departureInfo.innerHTML = `Departure: ${item.departureTime} - Destination: ${item.destination}`;
+                    container.appendChild(departureInfo);
+
+                    console.table(data);
+                });
+            } else {
+                container.textContent = "Ingen buser fundet.";
+                console.table(data);
+            }
         })
+        .catch((error) => {
+            console.error(error);
+
+            const container = document.getElementById('bus');
+            container.innerHTML = "Error: Kunne ikke fetch data.";
+        });
 }
 
-getRejseplan()
-
+getRejseplan();
